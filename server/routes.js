b4e2642473bd404cade96ac7c18d0b43d6c3e404
Banti Gheneti <banti@gheneti.com>
2015-03-06 19:01:49 -0500

    master

--- server/routes.js
+++ server/routes.js
@@ -1,11 +1,11 @@
 var routes=function(app){
     app.get('/', function(req, res){
-      res.render('index.html',{title:'wow'});
+      res.render('index.html',{title:'Detector Map'});
     });
 
-    app.get('/node/:name', function(req, res){
-      var name = req.params.name;
-      res.render('node.html', {title:name});
+    app.get('/node/:num', function(req, res){
+      var num = req.params.num;
+      res.render('node.html', {title:num});
     });
 }
 
